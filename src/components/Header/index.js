0a6551cad8d718cfda90eba7fab13885f2d2d677
felix eugenio <felixmacarenco@gmail.com>
2021-10-21 22:01:53 +0100

    rocketshoes

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ import { Container } from './styles';
+ import logo from '../../assets/images/logo.svg';
+
+function Header() {
+  return <img src={logo} alt="Rocketshoes"/>
+
+
+}
+
+export default Header;
