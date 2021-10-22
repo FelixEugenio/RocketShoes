94d073920ded305d6be816a169f19b8fa65fe28b
felix eugenio <felixmacarenco@gmail.com>
2021-10-22 22:42:51 +0100

    rocketshoes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,107 +1,51 @@
-import React from 'react';
+import React , {Component} from 'react';
 import {MdAddShoppingCart} from 'react-icons/md';
+import api from '../../services/api';
 
  import { ProductList } from './styles';
 
-export default function home() {
-  return (
-      <ProductList>
+export default class  Home extends Component {
 
+    state = {
+        products:[],
+    };
 
-          <li>
-              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
-              <strong>Tenis Muito Bonito</strong>
-              <span>22000 kz</span>
+    async componentDidMount(){
+        const response = await api.get('products');
 
-              <button type="button">
-                  <div>
-                      <MdAddShoppingCart size={16} color="#fff" /> 3
+        this.setState({products:response.data});
 
-                  </div>
+    }
 
-                  <span>ADICIONAR AO CARRINHO</span>
+  render(){
+
+    const {products} = this.state;
+    return (
+        <ProductList>
+            { products.map(product => (
+
+            <li key={product.id}>
+            <img src={product.image}/>
+            <strong>{product.title}</strong>
+            <span>{product.price} kz</span>
+
+             <button type="button">
+             <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
               </button>
-          </li>
+              </li>
 
-          <li>
-              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
-              <strong>Tenis Muito Bonito</strong>
-              <span>22000 kz</span>
-
-              <button type="button">
-                  <div>
-                      <MdAddShoppingCart size={16} color="#fff" /> 3
-
-                  </div>
-
-                  <span>ADICIONAR AO CARRINHO</span>
-              </button>
-          </li>
-
-          <li>
-              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
-              <strong>Tenis Muito Bonito</strong>
-              <span>22000 kz</span>
-
-              <button type="button">
-                  <div>
-                      <MdAddShoppingCart size={16} color="#fff" /> 3
-
-                  </div>
-
-                  <span>ADICIONAR AO CARRINHO</span>
-              </button>
-          </li>
-
-          <li>
-              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
-              <strong>Tenis Muito Bonito</strong>
-              <span>22000 kz</span>
-
-              <button type="button">
-                  <div>
-                      <MdAddShoppingCart size={16} color="#fff" /> 3
-
-                  </div>
-
-                  <span>ADICIONAR AO CARRINHO</span>
-              </button>
-          </li>
-
-          <li>
-              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
-              <strong>Tenis Muito Bonito</strong>
-              <span>22000 kz</span>
-
-              <button type="button">
-                  <div>
-                      <MdAddShoppingCart size={16} color="#fff" /> 3
-
-                  </div>
-
-                  <span>ADICIONAR AO CARRINHO</span>
-              </button>
-          </li>
-
-          <li>
-              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
-              <strong>Tenis Muito Bonito</strong>
-              <span>22000 kz</span>
-
-              <button type="button">
-                  <div>
-                      <MdAddShoppingCart size={16} color="#fff" /> 3
-
-                  </div>
-
-                  <span>ADICIONAR AO CARRINHO</span>
-              </button>
-          </li>
+            ))}
 
 
-      </ProductList>
 
+        </ProductList>
 
-  );
+    );
+  }
 }
 
