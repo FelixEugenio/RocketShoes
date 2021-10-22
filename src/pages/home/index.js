1079351d9c87a499f088d293b47c9248ef976d66
felix eugenio <felixmacarenco@gmail.com>
2021-10-22 13:50:41 +0100

    rocketshoes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,118 @@
 import React from 'react';
+import {MdAddShoppingCart} from 'react-icons/md';
 
-// import { Container } from './styles';
+ import { ProductList } from './styles';
 
-function home() {
-  return <h1>Pagina home</h1>;
+export default function home() {
+  return (
+      <ProductList>
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+
+          <li>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-air-max-sc-feminino/26/HZM-5147-026/HZM-5147-026_zoom2.jpg?ts=1630603638&ims=326x" alt="Tenis"/>
+              <strong>Tenis Muito Bonito</strong>
+              <span>R$129</span>
+
+              <button type="button">
+                  <div>
+                      <MdAddShoppingCart size={16} color="#fff" /> 3
+
+                  </div>
+
+                  <span>ADICIONAR AO CARRINHO</span>
+              </button>
+          </li>
+      </ProductList>
+
+
+  );
 }
 
-export default home;
